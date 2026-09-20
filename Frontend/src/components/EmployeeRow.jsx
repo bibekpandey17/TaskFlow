55cd210f0a8a2f1ac71584f3b6e60aca50e70b4f
Bibek Pandey <bibekpandey0317@gmail.com>
2026-09-20 18:23:38 +0545

    Update EmployeeRow.jsx

--- Frontend/src/components/EmployeeRow.jsx
+++ Frontend/src/components/EmployeeRow.jsx
@@ -4,7 +4,7 @@ const statusStyles = {
   Active: "bg-emerald-50 text-emerald-600",
   "On Leave": "bg-amber-50 text-amber-600",
 };
-
+console.log("employee object:", employee);
 export default function EmployeeRow({ employee, onDeleteClick }) {
   return (
     <tr className="border-b border-slate-100 last:border-0">
@@ -49,9 +49,9 @@ export default function EmployeeRow({ employee, onDeleteClick }) {
       {/* Actions */}
       <td className="text-xs font-medium px-2.5 py-1 rounded-full">
 
-        {/* Edit */}
+       {/* Edit */}
         <Link
-          to={`/admin-dashboard/employee-details/${employee._id?.toString() || employee.id || employee._id}/edit`}
+          to={`/admin-dashboard/employee-details/${employee._id || employee.id || employee.employeeId || employee.uid || ''}/edit`}
           className="px-3 py-1.5 bg-purple-100 text-[#9333EA] hover:bg-purple-200 rounded-md text-xs font-medium"
         >
           Edit
